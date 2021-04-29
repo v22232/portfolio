ed75d163b895e4c00e850268dd66bad2a76da20d
김부희 <v@gimbuhuiui-MacBookPro.local>
2021-04-29 15:47:15 +0900

    Add animation on Main

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import { AppShell } from '../components/common';
 
@@ -6,8 +7,37 @@ import '../styles/globals.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <AppShell>
-            <Component {...pageProps} />
-        </AppShell>
+        <>
+            <Head>
+            <meta charSet='UTF-8' />
+                <link rel='icon' type='image/x-icon' href='/icons/ico-logo.png' />
+
+                <meta httpEquiv='X-UA-Compatible' content='ie=edge' />
+                <meta
+                    name='viewport'
+                    content='user-scalable=no,width=device-width,initial-scale=1'
+                />
+                <meta name='description' content="Vuhee's portfolio" />
+                <meta property='og:type' content='website' />
+                <meta property='og:title' content='portfolio' />
+                <meta property='og:description' content="Vuhee's portfolio" />
+                <meta property='og:image' content='/icons/ico-logo.png' />
+                <meta property='og:url' content='https://vuhee.vercel.app' />
+
+                <title>Vuhee's portfolio</title>
+
+                <link
+                    href='https://fonts.googleapis.com/css?family=Noto+Sans+KR:300,400,500,700,900|Open+Sans:400,700&display=swap'
+                    rel='stylesheet'
+                />
+                <link
+                    href='https://fonts.googleapis.com/css2?family=Quantico:wght@400;700&display=swap'
+                    rel='stylesheet'
+                />
+            </Head>
+            <AppShell>
+                <Component {...pageProps} />
+            </AppShell>
+        </>
     );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,35 @@
+import cx from 'classnames';
+
 import style from '../styles/page/indexPage.module.scss';
 
 export default function indexPage() {
     return (
         <div className={style.indexPage}>
             <div className='group'>
-                tttt
+                <div className={style.layer}>
+                    <h2 className={style.part__top}>
+                        PORT
+                        <br />
+                        FOLIO
+                    </h2>
+                    <span className={cx([style.part__left, style.part__date])}>
+                        2017 TO 2021
+                    </span>
+                    <span
+                        className={cx([style.part__left, style.part__subject])}
+                    >
+                        PUBLISHER & FRONT-END DEVELOPER
+                    </span>
+                    <span
+                        className={cx([style.part__bottom, style.part__name])}
+                    >
+                        Buhee Kim
+                    </span>
+                    <ul className={style.part__menu}>
+                        <li>01. INFO</li>
+                        <li>02. CAREER</li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
