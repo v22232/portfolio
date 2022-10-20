a8a839e0de1f7bb26dad4e6fd724496c027492b0
김부희 <v@gimbuhuiui-MacBookPro.local>
2022-10-20 22:33:00 +0900

    테마색상 변경, 메인 애니메이션 시간 수정(빠르게)

--- components/common/Frame/index.tsx
+++ components/common/Frame/index.tsx
@@ -64,7 +64,7 @@ export default function Frame({
                     <ul>
                         <li>
                             <button onClick={() => _onClick('/info')}>
-                                01. INTRO
+                                01. INFO
                             </button>
                         </li>
                         <li>
